69a071554a1803311ddda630fac2707f5830cbde
Alan Barbosa <alanzinhob@hotmail.com>
2024-04-08 13:16:38 -0300

    08_04

--- components/Brands/SingleBrand.tsx
+++ components/Brands/SingleBrand.tsx
@@ -8,6 +8,7 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
   const { image, href, name, imageLight, id } = brand;
 
   return (
+    <div className="flex flex-col justify-items-center items-center gap-2">
       <motion.a
         variants={{
           hidden: {
@@ -29,19 +30,30 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
         className="animate_top mx-w-full relative block h-10 w-[98px]"
       >
         <Image
-          className="opacity-65 transition-all duration-300 hover:opacity-100 dark:hidden"
+          className="flex fixed opacity-65 transition-all duration-300 hover:opacity-100 dark:hidden"
           src={image}
           alt={name}
           fill
         />
         <Image
-          className="hidden opacity-50 transition-all duration-300 hover:opacity-100 dark:block"
+          className="hidden opacity-50 transition-all duration-300  hover:opacity-100 dark:block"
           src={imageLight}
           alt={name}
           fill
         />
       </motion.a>
-  
+      <Link href={href}>
+        <p className="flex justify-items-center items-center opacity-65 transition-all cursor-pointer duration-300 hover:opacity-100 cursor-pointer dark:hidden">
+          {name}
+        </p>
+      </Link>
+      <Link href={href}>
+        <p className="hidden flex justify-items-center items-center opacity-65 transition-all cursor-pointer duration-300 hover:opacity-100 dark:block">
+          {name}
+        </p>
+      </Link>
+    </div>
+
   );
 };
 
